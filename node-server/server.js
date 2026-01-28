543e810a6fc1e7ddfb5b6ac6b93d00551f9800bd
Emmanuel <emmanuelchibuike482@gmail.com>
2026-01-28 16:06:30 +0100

    Update server.js

--- node-server/server.js
+++ node-server/server.js
@@ -60,6 +60,10 @@ app.get("/sitemap.xml", async (req, res) => {
         }
       });
 
+      console.log("Fetching posts from Appwrite...");
+console.log("API URL:", queryUrl);
+
+
       const data = await response.json();
       const documents = data.documents || [];
       allPosts.push(...documents);
@@ -188,6 +192,7 @@ res.status(200);
 app.listen(PORT, () => console.log("OG server running on port", PORT));
 const server = app.listen(3000);
 server.timeout = 120000; // Sets timeout to 2 minutes
+
 
 
 

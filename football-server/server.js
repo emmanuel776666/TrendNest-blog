41d3d61492da7e74059aa2accfb4a9e2c943b3fe
Emmanuel <emmanuelchibuike482@gmail.com>
2026-05-16 16:19:23 +0100

    Refactor football match live updates logic

--- football-server/server.js
+++ football-server/server.js
@@ -324,17 +324,4 @@ getLiveMatches();
 
 console.log("Football bot running...");
 
-    const express = require("express");
-const app = express();
 
-const PORT = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-  res.send("Football bot is running...");
-});
-
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
-});
-
-});
